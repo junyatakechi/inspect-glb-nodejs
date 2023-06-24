8e372c65b67f40e4e6d16854633fe4fce7d81c8e
junyatakechi <takechi48j@gmail.com>
2023-06-24 21:31:03 +0900

    追加: ディレクトリ事のカウント

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,19 +1,46 @@
-import { NodeIO } from '@gltf-transform/core';
+import { NodeIO, Logger } from '@gltf-transform/core';
+import fs from 'fs';
+import path from 'path';
 
-const io = new NodeIO();
+// カスタムロガーを作成します。これは警告を無視し、エラーのみを出力します。
+const customLogger = {
+    debug: (message?: any, ...optionalParams: any[]): void => {},
+    info: (message?: any, ...optionalParams: any[]): void => {},
+    warn: (message?: any, ...optionalParams: any[]): void => {},
+    error: (message?: any, ...optionalParams: any[]): void => {
+        console.error(message, ...optionalParams);
+    },
+} as Logger;
 
-async function countMaterials(filePath: string): Promise<void> {
+const io = new NodeIO().setLogger(customLogger);
+
+async function countMaterials(filePath: string): Promise<number> {
     try {
         const document = await io.read(filePath);
-
         const materials = document.getRoot().listMaterials();
-
-        console.log(`The number of materials: ${materials.length}`);
+        return materials.length;
     } catch (err) {
         console.error(err);
+        return 0;
     }
 }
 
-const filePath = process.argv[2]; // ファイルパスをコマンドライン引数から取得
+async function countMaterialsInDirectory(dirPath: string): Promise<void> {
+    let totalMaterials = 0;
 
-countMaterials(filePath);
+    const files = fs.readdirSync(dirPath);
+    for (const file of files) {
+        const filePath = path.join(dirPath, file);
+        if (fs.statSync(filePath).isFile() && path.extname(filePath) === '.glb') {
+            const materialCount = await countMaterials(filePath);
+            console.log(`File: ${filePath}, Materials: ${materialCount}`);
+            totalMaterials += materialCount;
+        }
+    }
+
+    console.log(`Total number of materials in directory: ${totalMaterials}`);
+}
+
+const dirPath = process.argv[2]; // ディレクトリパスをコマンドライン引数から取得
+
+countMaterialsInDirectory(dirPath);
